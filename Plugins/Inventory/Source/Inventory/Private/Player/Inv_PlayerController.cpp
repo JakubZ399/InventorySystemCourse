955bec7b61794edc5aeeab3ec9d75f5683ccadeb
kubaz <zurawikjakub@gmail.com>
2025-11-30 15:16:58 +0100

    Inventory Plugin Upload
    Tracking For Item (1x10)

---
+++ Plugins/Inventory/Source/Inventory/Private/Player/Inv_PlayerController.cpp
@@ -0,0 +1,90 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/Inv_PlayerController.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputMappingContext.h"
+#include "Blueprint/UserWidget.h"
+#include "Widgets/HUD/Inv_HUDWidget.h"
+
+AInv_PlayerController::AInv_PlayerController()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	TraceLenght =  500.f;
+}
+
+void AInv_PlayerController::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	TraceForItem();
+}
+
+void AInv_PlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	if (IsValid(Subsystem))
+	{
+		for (UInputMappingContext* CurrentContext : DefaultIMCs)
+		{
+			Subsystem->AddMappingContext(CurrentContext, 0);	
+		}
+	}
+
+	CreateHUDWidget();
+}
+
+void AInv_PlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+	EnhancedInputComponent->BindAction(PrimaryInteractionAction, ETriggerEvent::Started, this, &AInv_PlayerController::PrimaryInteract);
+}
+
+void AInv_PlayerController::PrimaryInteract()
+{
+}
+
+void AInv_PlayerController::CreateHUDWidget()
+{
+	if (!IsLocalController()) return;
+	HUDWidget = CreateWidget<UInv_HUDWidget>(this, HUDWidgetClass);
+	if (IsValid(HUDWidget))
+	{
+		HUDWidget->AddToViewport();
+	}
+}
+
+void AInv_PlayerController::TraceForItem()
+{
+	int32 x, y;
+	GetViewportSize(x, y);
+
+	FVector TraceStart, Forward;
+	if (!DeprojectScreenPositionToWorld(x / 2, y / 2, TraceStart, Forward)) return;
+
+	const FVector TraceEnd = TraceStart + Forward * TraceLenght;
+	FHitResult HitResult;
+	GetWorld()->LineTraceSingleByChannel(HitResult, TraceStart, TraceEnd, ItemTraceChannel);
+
+	LastActor = ThisActor;
+	ThisActor = HitResult.GetActor();
+	
+	if (ThisActor == LastActor) return;
+
+	if (ThisActor.IsValid())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Hit"));
+	}
+
+	if (LastActor.IsValid())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Lost"));
+	}
+	
+}
